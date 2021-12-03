a5ed65bf7e3093674abee60579437ef428d2cf81
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2021-12-03 00:14:55 -0500

    Add a general function that parses JSON and produces a pandas DataFrame.

--- parse_json.py
+++ parse_json.py
@@ -1,5 +1,6 @@
-from constants import DIRECTORY
+from constants import DIRECTORY, FOURSQUARE_LEGEND_DISCREPANCIES, YELP_LEGEND_DISCREPANCIES, GOOGLE_LEGEND_DISCREPANCIES
 import json
+import pandas as pd
 
 
 def json_from_file(file_name: str, directory: str = DIRECTORY) -> dict:
@@ -15,3 +16,68 @@ def json_from_file(file_name: str, directory: str = DIRECTORY) -> dict:
 
     return query_dict
 
+
+def parse_object_to_df(json_object: dict, results_key: str = 'results', legend_discrepencies: dict = None) -> pd.DataFrame:
+    """
+    Takes in a JSON object, key to the results list, and a legend which is applied to extract
+    the desired data. The resulting object is then converted to a pandas DataFrame and returned.
+
+    parameters:
+        json_object: dict = the JSON object to parse
+        results_key: str = the key to the results list
+        legend_exceptions: dict = dict('column_key': 'json_key_to_replace')
+    """
+    _legend_template = {
+        'Name': 'name',
+        'Price': 'price',
+        'Type': 'types',
+        'Rating': 'rating',
+        'RatingCount': 'review_count',
+        'Source': 'unknown'
+    }
+
+    # Adjust discrepencies in the key names of the data to be parsed
+    for key, value in legend_discrepencies.items():
+        _legend_template[key] = value
+
+    # set the column names based on legend values
+    _pre_df = {column_key: [] for column_key in _legend_template}
+    _key_list = json_object[results_key]
+    print(len(_key_list))
+    # As long as the key list isn't empty, pop out the last element and store it.
+    while _key_list:
+        _current = _key_list.pop()
+        # Use the corresponding key_value pairs in the legend to add to the column.
+        for column_key, json_key in _legend_template.items():
+            if column_key != 'Source':
+                _pre_df[column_key].append(_current.get(json_key))
+            else:
+                _pre_df[column_key].append(json_key)
+
+    return pd.DataFrame.from_dict(_pre_df)
+
+
+def main():
+    # ----------------------------------Preview Foursquare-----------------------------------
+    # json_obj = json_from_file('Foursquare_restaurants.json')
+    # df = parse_object_to_df(
+    #     json_obj, legend_discrepencies=FOURSQUARE_LEGEND_DISCREPANCIES)
+    # print(df.head(10))
+
+    # ------------------------------------Preview Yelp--------------------------------------
+    # json_obj = json_from_file('Yelp_restaurants.json')
+    # df = parse_object_to_df(
+    #     json_obj, 'businesses', legend_discrepencies=YELP_LEGEND_DISCREPANCIES)
+    # print(df.head(10))
+
+    # -----------------------------------Preview Google--------------------------------------
+    # json_obj = json_from_file('Google_restaurants.json')
+    # df = parse_object_to_df(
+    #     json_obj, legend_discrepencies=GOOGLE_LEGEND_DISCREPANCIES)
+    # print(df.head(10))
+    # NOTE: Google results are currently divided across 3 files.
+    pass
+
+
+if __name__ == '__main__':
+    main()
