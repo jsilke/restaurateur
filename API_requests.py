1bdc68c80267294e9d7613d267d65193748ca835
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2021-12-03 16:04:52 -0500

    Change naming to be more consistent with constants.

--- API_requests.py
+++ API_requests.py
@@ -36,45 +36,45 @@ def store_response_json(json_object: dict, file_name: str,
 
 # ------------------------------------------Higher-level functions-----------------------------------
 
-def get_response_from_endpoint_and_store_json(endpoint_parameters: dict) -> None:
+def get_response_from_endpoint_and_store_json(endpoint_dict: dict) -> None:
     """
     Makes get requests to provided API endpoints using provided parameters and saves 
     the JSON responses. See the below for necessary key names and value examples.
 
     parameter:
-        endpoint_parameters: dict   {
+        endpoint_dict: dict   {
                                      'file': 'filename.json'
                                      'endpoint': 'https://api/search'
                                      'params': dict(query_parameters)
                                      'headers': dict(headers)
                                     }
     """
-    _json_object = get_json(url=endpoint_parameters.get('endpoint'),
-                            parameters=endpoint_parameters.get('params'),
-                            headers=endpoint_parameters.get('headers'))
+    _json_object = get_json(url=endpoint_dict.get('endpoint'),
+                            parameters=endpoint_dict.get('params'),
+                            headers=endpoint_dict.get('headers'))
 
     store_response_json(_json_object,
-                        file_name=endpoint_parameters.get('file'))
+                        file_name=endpoint_dict.get('file'))
 
 
-def make_requests_and_store_json_responses(all_endpoint_parameters: list[dict] = ALL_ENDPOINTS) -> None:
+def make_requests_and_store_json_responses(all_endpoint_dict: list[dict] = ALL_ENDPOINTS) -> None:
     """
-    Takes in a list of endpoint_parameters dictionaries and calls get_response_from_endpoint_and_store_json
-    for each dictionary.
+    Takes in a list of endpoint_dicts and calls get_response_from_endpoint_and_store_json for each 
+    dictionary.
     """
-    for endpoint_dictionary in all_endpoint_parameters:
+    for endpoint_dictionary in all_endpoint_dict:
         get_response_from_endpoint_and_store_json(endpoint_dictionary)
 
 
 # -------------------------------------For Google's pagination-----------------------------------------------
 
-def get_next_page(next_page_token: str, google_endpoint_parameters: dict = ALL_ENDPOINTS[-1]):
+def get_next_page(next_page_token: str, google_endpoint_dict: dict = ALL_ENDPOINTS[-1]):
     """
     Takes in a next_page_token from a previous API search and uses it 
     """
-    json_object = get_json(url=google_endpoint_parameters.get('endpoint'),
+    json_object = get_json(url=google_endpoint_dict.get('endpoint'),
                            parameters={'pagetoken': next_page_token,
-                                       'key': google_endpoint_parameters['params']['key']})
+                                       'key': google_endpoint_dict['params']['key']})
 
     return json_object
 
@@ -83,7 +83,7 @@ def main():
     # Sends GET requests using the endpoints and their parameters included in the list and stores the json objects in files.
     make_requests_and_store_json_responses()
 
-    # TODO automate Google pagination.
+    # TODO automate Foursquare and Google pagination.
 
     # Google page 2
     # json_object = get_next_page("Aap_uECe41Ay0uxoHNwt3D0AN30zMnrgm21rISDF7Kxf6OSzJBXEiHKcz7wunxdmzQvn8Pt0iJZAmGeP00NuXYe-ZeDAuq1m8MjmXkpg5WY1d7B6YzZVDdV-yerFsVi1_4cKd4hJ98fa_hO_fO4mBQ_enThE7A6cTGFPC6Aphcy6DjRv1g2v27pOGXLaTqdRKQy3tz5i54SDMbnMOwHjopbRkZ-nDpSLYJJBzGcZMbn4m8b9w0Y8s6KdTkKHMmGxjW9pqUTG1sdvHoKLAoiV2v01aHQxUn23Z8cccHXT0tGgv-6GiZTZIUy7yR2vjXJ8vLnFGJPW8ilnr4D5uMIQO-GyKT_B3lelTMOydD1_VYf7gwTPNFUlZhkTqnA4p5pTGJDwwHGXq_smtMNptrXGPxMklHD6kfrZQCkw9WDBRqgbREUfpjLaUQqs5QuH9M1I")
